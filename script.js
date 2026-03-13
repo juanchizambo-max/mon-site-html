cd73aee2ea66bf23903a18a28b7bee6afd85cad4
juanchizambo-max <juanchizambo@gmail.com>
2026-03-13 16:40:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-alert("Welcome to NiceGames")
 
-let name = "Juan";
-let age = 3;
 
-console.log(name);
-console.log(age);
-const button = document.getElementById("myButton");
+window.onload = function(){
 
-button.addEventListener("click", function (
-});
-const title = document.querySelector("h1");
-title.style.color = "green";
-title.textContent = "JavaScript is working!";
 function toggleMenu(){
 document.querySelector(".nav-links").classList.toggle("active");
 }
 
+window.toggleMenu = toggleMenu;
+
+window.jouer = function(){
+let nombre = Math.floor(Math.random()*10)+1;
+document.getElementById("resultat").textContent =
+"Le nombre est : " + nombre;
+}
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+let x = 250;
+let y = 150;
+let dx = 3;
+let dy = 3;
+
+function pong(){
+ctx.clearRect(0,0,500,300);
+
+ctx.fillStyle = "white";
+ctx.fillRect(x,y,10,10);
+
+x += dx;
+y += dy;
+
+if(x < 0 || x > 490) dx = -dx;
+if(y < 0 || y > 290) dy = -dy;
+}
+
+setInterval(pong,20);
+
+}
