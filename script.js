2957dc7c9b786d3e0c407a1007632534b4f654cf
juanchizambo-max <juanchizambo@gmail.com>
2026-03-13 22:28:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,76 +13,11 @@ document.getElementById("resultat").textContent =
 "Le nombre est : " + nombre;
 }
 
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+/* PONG */
 
-let x = 250;
-let y = 150;
-let dx = 3;
-let dy = 3;
+let pongGame;
 
-let game;
-
-window.pong = function(){
-if(!game){
-game = setInterval(updatePong,20);
-}
-}
-
-function updatePong(){
-ctx.clearRect(0,0,500,300);
-
-ctx.fillStyle="white";
-ctx.fillRect(x,y,10,10);
-
-x+=dx;
-y+=dy;
-
-if(x<0 || x>490) dx=-dx;
-if(y<0 || y>290) dy=-dy;
-}
-  
-let snakeGame;
-
-function startSnake(){
-
-const canvas = document.getElementById("snake");
-const ctx = canvas.getContext("2d");
-
-let snake = [{x:200,y:200}];
-let dx = 20;
-let dy = 0;
-
-document.addEventListener("keydown", changeDirection);
-
-function changeDirection(e){
-if(e.key === "ArrowUp"){dx=0;dy=-20;}
-if(e.key === "ArrowDown"){dx=0;dy=20;}
-if(e.key === "ArrowLeft"){dx=-20;dy=0;}
-if(e.key === "ArrowRight"){dx=20;dy=0;}
-}
-
-function drawGame(){
-
-ctx.clearRect(0,0,400,400);
-
-snake.unshift({x:snake[0].x+dx,y:snake[0].y+dy});
-snake.pop();
-
-ctx.fillStyle="lime";
-
-snake.forEach(part=>{
-ctx.fillRect(part.x,part.y,20,20);
-});
-
-}
-
-snakeGame = setInterval(drawGame,100);
-
-}
-  let pongGame;
-
-function startPong(){
+window.startPong = function(){
 
 const canvas = document.getElementById("pongCanvas");
 const ctx = canvas.getContext("2d");
@@ -120,17 +55,14 @@ ctx.fillRect(10,paddleY,10,paddleHeight);
 ballX += ballDX;
 ballY += ballDY;
 
-/* rebond haut bas */
 if(ballY < 0 || ballY > canvas.height){
 ballDY = -ballDY;
 }
 
-/* rebond raquette */
 if(ballX < 20 && ballY > paddleY && ballY < paddleY+paddleHeight){
 ballDX = -ballDX;
 }
 
-/* rebond mur droite */
 if(ballX > canvas.width){
 ballDX = -ballDX;
 }
@@ -140,4 +72,46 @@ ballDX = -ballDX;
 pongGame = setInterval(draw,20);
 
 }
+
+/* SNAKE */
+
+let snakeGame;
+
+window.startSnake = function(){
+
+const canvas = document.getElementById("snake");
+const ctx = canvas.getContext("2d");
+
+let snake = [{x:200,y:200}];
+let dx = 20;
+let dy = 0;
+
+document.addEventListener("keydown", changeDirection);
+
+function changeDirection(e){
+if(e.key === "ArrowUp"){dx=0;dy=-20;}
+if(e.key === "ArrowDown"){dx=0;dy=20;}
+if(e.key === "ArrowLeft"){dx=-20;dy=0;}
+if(e.key === "ArrowRight"){dx=20;dy=0;}
+}
+
+function drawGame(){
+
+ctx.clearRect(0,0,400,400);
+
+snake.unshift({x:snake[0].x+dx,y:snake[0].y+dy});
+snake.pop();
+
+ctx.fillStyle="lime";
+
+snake.forEach(part=>{
+ctx.fillRect(part.x,part.y,20,20);
+});
+
+}
+
+snakeGame = setInterval(drawGame,100);
+
+}
+
 }
