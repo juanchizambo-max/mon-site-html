a5221310525f103ed9c99091350438ae87b0c7c6
juanchizambo-max <juanchizambo@gmail.com>
2026-03-13 16:50:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,27 +14,24 @@ document.getElementById("resultat").textContent =
 "Le nombre est : " + nombre;
 }
 
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+let game;
 
-let x = 250;
-let y = 150;
-let dx = 3;
-let dy = 3;
-
-function pong(){
-ctx.clearRect(0,0,500,300);
-
-ctx.fillStyle = "white";
-ctx.fillRect(x,y,10,10);
-
-x += dx;
-y += dy;
-
-if(x < 0 || x > 490) dx = -dx;
-if(y < 0 || y > 290) dy = -dy;
+window.pong = function(){
+if(!game){
+game = setInterval(updatePong,20);
+}
 }
 
-setInterval(pong,20);
+function updatePong(){
+ctx.clearRect(0,0,500,300);
 
+ctx.fillStyle="white";
+ctx.fillRect(x,y,10,10);
+
+x+=dx;
+y+=dy;
+
+if(x<0 || x>490) dx=-dx;
+if(y<0 || y>290) dy=-dy;
+}
 }
